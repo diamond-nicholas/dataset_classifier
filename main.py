dd8d37ba460565fd910b69010a04b35e79bb103d
Diamond Nicholas <diamondnicholas@apples-MacBook-Pro.local>
2022-05-22 10:07:57 +0100

    train algorithm and plot 2d scattered data

--- main.py
+++ main.py
@@ -1,45 +1,108 @@
-import streamlit as st
+import streamlit as st 
+import numpy as np 
+import matplotlib.pyplot as plt
 from sklearn import datasets
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-st.title('Welcome')
+
+
 st.write("""
-### Explore different classifier
-which one is the best
+# Explore different classifier and datasets
+Which one is the best?
 """)
 
-dataset_name = st.sidebar.selectbox('Select Dataset', ("Iris", "Breast Cancer", "Wine Dataset"))
-classifier_name = st.sidebar.selectbox('Select Classifier', ("KNN", "SVM", "Random Forest"))
+dataset_name = st.sidebar.selectbox(
+    'Select Dataset',
+    ('Iris', 'Breast Cancer', 'Wine')
+)
 
-def get_dataset(dataset_name):
-    if dataset_name == 'Iris': 
+st.write(f"## {dataset_name} Dataset")
+
+classifier_name = st.sidebar.selectbox(
+    'Select classifier',
+    ('KNN', 'SVM', 'Random Forest')
+)
+
+def get_dataset(name):
+    data = None
+    if name == 'Iris':
         data = datasets.load_iris()
-    elif dataset_name == 'Breast Cancer':
-        data = datasets.load_breast_cancer()
-    else:
+    elif name == 'Wine':
         data = datasets.load_wine()
+    else:
+        data = datasets.load_breast_cancer()
     X = data.data
     y = data.target
-    # st.write(y)
     return X, y
-X, y = get_dataset(dataset_name)
 
-st.write('Shape of the dataset', X.shape)
-st.write('Number of classes', len(np.unique(y)))
+X, y = get_dataset(dataset_name)
+st.write('Shape of dataset:', X.shape)
+st.write('number of classes:', len(np.unique(y)))
 
 def add_parameter_ui(clf_name):
     params = dict()
-    if clf_name == 'KNN': 
-        K = st.sidebar.slider('K', 1,15)
-        params["k"] = K
-    elif clf_name == "SVM":
-        C = st.sidebar.slider("C", 0.01,10.0)
-        params["C"] = C
+    if clf_name == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        params['C'] = C
+    elif clf_name == 'KNN':
+        K = st.sidebar.slider('K', 1, 15)
+        params['K'] = K
     else:
-        max_depth = st.sidebar.slider("max_depth", 2, 15)
-        n_estimators = st.sidebar.slider("n_estimators", 1, 100)
-        params["max_depth"] = max_depth
-        params["n_estimators"] = n_estimators
-        return params
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
+    return params
 
-add_parameter_ui(classifier_name)
+params = add_parameter_ui(classifier_name)
+
+def get_classifier(clf_name, params):
+    clf = None
+    if clf_name == 'SVM':
+        clf = SVC(C=params['C'])
+    elif clf_name == 'KNN':
+        clf = KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
+            max_depth=params['max_depth'], random_state=1234)
+    return clf
+
+clf = get_classifier(classifier_name, params)
+
+
+# classification
+X_train, X_test, y_train, y_test = train_test_split(X, y , test_size=0.2, random_state=1234)
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+
+
+# write to the page
+st.write(f"classifier = {classifier_name}")
+st.write(f"accuracy = {acc}")
+
+# plot
+
+pca = PCA(2)
+X_projected = pca.fit_transform(X)
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+fig = plt.figure()
+
+plt.scatter(x1,x2, c=y, alpha=0.8, cmap="viridis")
+plt.xlabel("principal component 1")
+plt.ylabel("principal component 2")
+plt.colorbar()
+
+
+# plt.show()
+st.pyplot()
